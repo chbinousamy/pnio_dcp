c83ce67505719fa927601de120cfa0f6e60db939
Vlada Lazepka <vlada.lazepka@codewerk.de>
2020-06-03 18:44:22 +0200

    small corrections

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,6 @@
+import os, sys
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../tests')
+sys.path.insert(0, myPath + '/../cw_dcp')
+
 from fixture.instance_dcp import instance_dcp
