703797069477a352f777e19e185b1629985a5a74
Katharina Flügel <katharina.fluegel@codewerk.de>
2021-01-26 17:44:12 +0100

    Add and fix docstrings
    - add extensive docstrings to all functions
    - fix previous docstrings where outdated
    - add inline comments where necessary

--- cw_dcp/error.py
+++ cw_dcp/error.py
@@ -1,6 +1,8 @@
 class DcpError(Exception):
+    """Base class of the errors thrown by this DCP lib."""
     pass
 
 
 class DcpTimeoutError(DcpError):
+    """Thrown if a timeout occurs withing this DCP lib."""
     pass
