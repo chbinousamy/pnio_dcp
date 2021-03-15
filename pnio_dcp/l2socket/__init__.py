02e47473d5ce0a5e49d36291a1799424b598840e
Katharina Flügel <katharina.fluegel@codewerk.de>
2021-03-15 14:45:33 +0100

    Switch socket based on the OS

--- pnio_dcp/l2socket/__init__.py
+++ pnio_dcp/l2socket/__init__.py
@@ -1,1 +1,9 @@
-from pnio_dcp.l2socket.l2socket import L2PcapSocket as L2Socket
+import os
+from .l2socket import L2PcapSocket
+
+if os.name == 'nt':
+    L2Socket = L2PcapSocket
+elif os.name == 'posix':
+    raise NotImplementedError(f"OS {os.name} is currently not supported.")
+else:
+    raise NotImplementedError(f"OS {os.name} is currently not supported.")
