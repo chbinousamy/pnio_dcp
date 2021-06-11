03d52c7eb4b2fc1f1e90e6f1eff2b1e3129404d0
Katharina Flügel <katharina.fluegel@codewerk.de>
2021-06-11 12:11:15 +0200

    Improve platform detection
    by using sys.platform instead of os.name

--- pnio_dcp/l2socket/__init__.py
+++ pnio_dcp/l2socket/__init__.py
@@ -3,12 +3,12 @@ Copyright (c) 2021 Codewerk GmbH, Karlsruhe.
 All Rights Reserved.
 License: MIT License see LICENSE.md in the pnio_dcp root directory.
 """
-import os
+import sys
 from .l2socket import L2PcapSocket
 
-if os.name == 'nt':
+if sys.platform == 'win32':
     L2Socket = L2PcapSocket
-elif os.name == 'posix':
-    raise NotImplementedError(f"OS {os.name} is currently not supported.")
+elif sys.platform.startswith('linux'):
+    raise NotImplementedError(f"Platform {sys.platform} is currently not supported.")
 else:
-    raise NotImplementedError(f"OS {os.name} is currently not supported.")
+    raise NotImplementedError(f"Platform {sys.platform} is currently not supported.")
