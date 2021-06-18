dfc9a609f91e1aab782d4ad8474377bf7069c62a
Katharina Flügel <katharina.fluegel@codewerk.de>
2021-06-18 15:30:33 +0200

    Fix linux socket: set protocol id

--- pnio_dcp/l2socket/l2socket.py
+++ pnio_dcp/l2socket/l2socket.py
@@ -35,7 +35,6 @@ class L2PcapSocket:
         :return: The next raw packet (or None if no packet has been received e.g. due to a timeout).
         :rtype: Optional(bytes)
         """
-        # Receive the next packet from pcap
         return self.pcap.get_next_packet()
 
     def send(self, data):
@@ -52,19 +51,25 @@ class L2PcapSocket:
 
 
 class L2LinuxSocket:
+    MTU = 0xffff
+    ETH_P_ALL = 3
 
-    def __init__(self, ip, interface=None):
-        self.socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
+    def __init__(self, ip=None, interface=None, filter=None, recv_timeout=1, protocol=None):
+        protocol = protocol or self.ETH_P_ALL
+        self.socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(protocol))
+        self.socket.settimeout(recv_timeout)
         self.socket.bind((interface, 0))
 
     def recv(self):
         """
-        Receive the next packet from pcap.
+        Receive the next packet from the socket.
         :return: The next raw packet (or None if no packet has been received e.g. due to a timeout).
         :rtype: Optional(bytes)
         """
-        ETH_FRAME_LEN = 1514
-        return self.socket.recv(ETH_FRAME_LEN)
+        try:
+            return self.socket.recv(self.MTU)
+        except socket.timeout:
+            return None
 
     def send(self, data):
         """
